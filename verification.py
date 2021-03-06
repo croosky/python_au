4e7828f83b4b0a5580f9420d7006143baebc7d0e
ddd <dd@gmail.com>
2021-03-06 16:54:30 +0300

    VERIFICATION-1021 Fixed

--- verification.py
+++ verification.py
@@ -3,10 +3,13 @@ import re
 
 TOKEN = 'f20683ab9b4227c373f132806b08b432fcc750c6'
 
+
 def get_headers():
     return {'Authorization': 'token ' + TOKEN}
 
-#repos = requests.get('https://api.github.com/users/croosky/repos', headers=get_headers()).json()
+
+repos = requests.get('https://api.github.com/users/croosky/repos', headers=get_headers()).json()
+
 
 class Github_requests:
     def __init__(self, owner='croosky', rep='python_au', state='all'):
@@ -29,9 +32,15 @@ class Github_requests:
 
     def commit_verification(self, message):
         errors = []
-        if not (re.match(r'LEETCODE-102', message) or re.match(r'GENERATOR-102', message) or re.match(r'HEXNUMBER-102', message) or re.match(r'TRIANGLE-102', message) or re.match(r'VERIFICATION-102', message) or re.match(r'ITERATOR-102', message)):
-            errors.append('Identificator should be LEETCODE/GENERATOR/HEXNUMBER/TRIANGLE/ITERATOR/VERIFICATION-1021/1022.')
-        if not (re.search(r'Added', message) or re.search(r'Refactored', message) or re.search(r'Deleted', message) or re.search(r'Fixed', message)):
+        if not (re.match(r'LEETCODE-102', message) or re.match(r'GENERATOR-102', message) or re.match(r'HEXNUMBER-102',
+                                                                                                      message) or re.match(
+                r'TRIANGLE-102', message) or re.match(r'VERIFICATION-102', message) or re.match(r'ITERATOR-102',
+                                                                                                message)):
+            errors.append(
+                'Identificator should be LEETCODE/GENERATOR/HEXNUMBER/TRIANGLE/ITERATOR/VERIFICATION-1021/1022.')
+        if not (re.search(r'Added', message) or re.search(r'Refactored', message) or re.search(r'Deleted',
+                                                                                               message) or re.search(
+                r'Fixed', message)):
             errors.append('State should be Added/Refactored/Deleted/Fixed.')
         return errors
 
@@ -42,9 +51,10 @@ class Github_requests:
             errors = self.commit_verification(message)
             if len(errors) > 0:
                 output += "Commit '{}' is wrong. {}\n".format(message, ''.join(errors))
-        print("Request - {}".format(pullrequest['url']))
-        print(output)
-        # output отправляется в pullrequest на гитхаб
+        body = {'body': output}
+        response = requests.post(
+            'https://api.github.com/repos/{}/{}/issues/{}/comments'.format(self.owner, self.rep, pullrequest['number']),
+            headers=get_headers(), data=json.dumps(body))
 
 
 def main():
@@ -56,6 +66,7 @@ def main():
     request = Github_requests()
     pull_requests = request.get_all_pullrequests()
     for pull_request in pull_requests:
+        # print(pull_requests)
         request.error_message(pull_request)
 
 
